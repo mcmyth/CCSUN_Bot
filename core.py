cac2d442c84d4f95c6e949a75ae8035524900ee3
MC_Myth <1440126177@qq.com>
2021-10-08 19:36:33 +0800

    :bulb: add comments

--- core.py
+++ core.py
@@ -20,7 +20,7 @@ app = Mirai(f"mirai://{mirai_api_http_locate}?authKey={authKey}&qq={qq}")
 ccsun_group = configManager.config["ccsunGroup"]
 write_log("[Notice] CCSUN-Bot已启动", 3)
 
-
+# 提交当天流量数据
 async def auto_update():
     try:
         info = await updateBandwidth()
@@ -29,7 +29,7 @@ async def auto_update():
         print(e)
         write_log(e, 3)
 
-
+# 每日0点定时任务
 @scheduler.scheduled_job('cron', hour='0', minute='0', second='0')
 def run_timer():
     asyncio.run(auto_update())
@@ -64,14 +64,17 @@ async def run_command(message_type: str, data: dict):
             cq_message = CQEncoder.messageChainToCQ(message)
             write_log(f"[{member.id}]{cq_message}", 1)
             command = commandDecode(cq_message)
+            # 订阅回复检测
             if is_number(cq_message) and querySubTask(member.id, True):
                 cq_message = f'订阅{cq_message}'
             else:
                 querySubTask(member.id, True)
+            # 登录指令
             if cq_message == "登录":
                 CCSUN.Login()
                 info = '[Notice]\n已登录'
                 await mirai_app.sendGroupMessage(ccsun_group, info)
+            # 流量指令
             if cq_message == "流量":
                 info = CCSUN.getBandwidthStr()
                 if info.find('Error') != -1:
@@ -79,16 +82,20 @@ async def run_command(message_type: str, data: dict):
                     CCSUN.refreshToken()
                     info = CCSUN.getBandwidthStr()
                 await mirai_app.sendGroupMessage(ccsun_group, info)
+            # 订阅指令
             if cq_message[:2] == "订阅":
                 keyword_len = 2
                 num = None
+                # 字数大于2并且后面的内容为数字则获取订阅链接
                 if len(cq_message) > keyword_len:
                     num = cq_message[keyword_len:]
                     if is_number(num): info = CCSUN.getSubscribeForMenu(num)
                     if not is_number(num): return
                 else:
+                    # 获取订阅菜单
                     addSubTask(member.id)
                     info = CCSUN.getSubscribeForMenu()
+                # 获取失败重试一次
                 if info == '':
                     CCSUN.Login()
                     info = CCSUN.getSubscribeForMenu(num)
@@ -97,21 +104,27 @@ async def run_command(message_type: str, data: dict):
                 else:
                     info = info
                 await mirai_app.sendGroupMessage(ccsun_group, info)
+            # 图表指令
             if cq_message[:2] == "图表" or cq_message[:4] == "离线图表":
                 is_offline = True if cq_message[:2] == "图表" else False
                 keyword_len = 2 if cq_message[:2] == "图表" else 4
                 day = ""
+                # 获取指令后面的数字,如没有则默认为7
                 if len(cq_message) >= keyword_len:
                     day = cq_message[keyword_len:]
                     if not is_number(day):
                         day = "7"
+                # 不允许大于180天的请求
                 if int(day) > 180:
                     day = "180"
                     info = "[Notice]\n最大查询过去180天的数据"
                     await mirai_app.sendGroupMessage(ccsun_group, info)
+                # 获取图表
                 image_path = CCSUN.getChart(str(source.id), day, is_offline)
+                # 获取失败重试一次
                 if image_path == '':
                     image_path = CCSUN.getChart(str(source.id), day, is_offline)
+                # 获取成功发送图表,失败则提示
                 if image_path != '':
                     await mirai_app.sendGroupMessage(ccsun_group, [Image.fromFileSystem(image_path)])
                     os.remove(image_path)
@@ -119,7 +132,9 @@ async def run_command(message_type: str, data: dict):
                 else:
                     info = "[getChart Error] 获取图表失败"
                     await mirai_app.sendGroupMessage(ccsun_group, [Image.fromFileSystem(image_path)])
+            # ccsun指令集
             if command[0].lower() == "/ccsun":
+                # 强制提交当天流量数据
                 if len(command) >= 1:
                     if command[1].lower() == "update":
                         info = await updateBandwidth()
